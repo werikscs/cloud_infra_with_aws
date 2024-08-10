b2c0ea4b0ca096d8b5ba77cc6322a9cbc52099e3
Weriks Santos <werikscs@gmail.com>
2024-08-10 17:37:45 +0000

    chore: add prisma

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import 'dotenv/config'
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prismaClient = new PrismaClient()
 
 export const app = express()
 
@@ -7,4 +10,21 @@ app.use(express.json())
 
 app.get('/healthcheck', (req, res) => {
   res.status(200).json({ status: 'ok' })
+})
+
+app.get('/users', async (req, res) => {
+  const users = await prismaClient.user.findMany()
+  await prismaClient.$disconnect()
+  res.status(200).json({ users })
+})
+
+app.post('/users', async (req, res) => {
+  const user = await prismaClient.user.create({
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+    },
+  })
+  await prismaClient.$disconnect()
+  res.status(201).json({ user })
 })
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import { app } from '.'
 
 app.listen(process.env.APP_PORT_DEV, () => {
   return console.log(
-    `Express is listening at http://localhost:${process.env.APP_PORT_DEV}`,
+    `Express is listening at http://host.docker.internal:${process.env.APP_PORT_DEV}`,
   )
 })
