790b2f2dd4721851663815c22bbdf7c59ff59b2e
Weriks Santos <werikscs@gmail.com>
2024-08-14 19:26:40 +0000

    chore: disable file parallelism in test configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    fileParallelism: false,
     coverage: {
       provider: 'istanbul',
       exclude: ['build', 'tests'],
