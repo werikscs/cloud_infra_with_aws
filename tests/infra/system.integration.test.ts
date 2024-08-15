964cb5d062de93cf9b27aa061dee6cadab6cb6fc
Weriks Santos <werikscs@gmail.com>
2024-08-15 17:39:18 +0000

    test: add a describe block to test

--- tests/infra/system.integration.test.ts
+++ tests/infra/system.integration.test.ts
@@ -1,6 +1,8 @@
 import request from 'supertest'
 import { app } from '@/src'
 
-it('should get 200 from healthcheck route', async () => {
-  await request(app).get('/system/healthcheck').expect(200)
+describe('ROUTE: System', async () => {
+  it('should get 200 from healthcheck route', async () => {
+    await request(app).get('/system/healthcheck').expect(200)
+  })
 })
