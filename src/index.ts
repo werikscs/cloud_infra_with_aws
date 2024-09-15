eca636d7c5d0c1c34ee3783571386b63de3c5c7f
Weriks Santos <werikscs@gmail.com>
2024-09-15 23:31:06 +0000

    chore: add morgan middleware for logging

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import 'express-async-errors'
 import express from 'express'
+import morgan from 'morgan'
 
 import { handleAppError } from './middlewares/handleAppError.mdw'
 import { router } from './routes'
@@ -8,5 +9,6 @@ import { router } from './routes'
 export const app = express()
 
 app.use(express.json())
+app.use(morgan('tiny'))
 app.use(router)
 app.use(handleAppError)
