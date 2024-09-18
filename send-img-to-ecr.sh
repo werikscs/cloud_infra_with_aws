4e14adb772dd3f13608de805a70274c0bd6fe5af
Weriks Santos <werikscs@gmail.com>
2024-09-18 22:57:37 +0000

    feat: add script to build and push Docker image to ECR

---
+++ send-img-to-ecr.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Verifica se o parâmetro foi fornecido
+if [ -z "$1" ]; then
+  echo "Uso: ./build.sh SEU_REGISTRY"
+  exit 1
+fi
+
+ECR_REGISTRY="$1"
+
+# Executa o login no ECR
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REGISTRY
+
+# Constrói a imagem Docker
+docker build -t crud .
+
+# Marca a imagem com o registro ECR
+docker tag crud:latest $ECR_REGISTRY/crud:latest
+
+# Envia a imagem para o registro ECR
+docker push $ECR_REGISTRY/crud:latest
