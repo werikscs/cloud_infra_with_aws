485c3a661681aa7811d0a4b508a5f2ff7cab4f32
Weriks Santos <werikscs@gmail.com>
2024-09-18 23:02:32 +0000

    refactor: rename build_to_ecr.sh script and update Docker image name and tags

--- build_to_ecr.sh
+++ build_to_ecr.sh
@@ -12,10 +12,10 @@ ECR_REGISTRY="$1"
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REGISTRY
 
 # Constrói a imagem Docker
-docker build -t crud .
+docker build -t cloud_infra_with_aws .
 
 # Marca a imagem com o registro ECR
-docker tag crud:latest $ECR_REGISTRY/crud:latest
+docker tag cloud_infra_with_aws:latest $ECR_REGISTRY/cloud_infra_with_aws:latest
 
 # Envia a imagem para o registro ECR
-docker push $ECR_REGISTRY/crud:latest
+docker push $ECR_REGISTRY/cloud_infra_with_aws:latest
